9ef5fc18bad986cfdfba6eda9a989ecbe7ea57c6
Kobe Li <likobe23@gmail.com>
2022-03-01 18:03:36 -0800

    passed store to react app

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,7 +7,7 @@ export const store = configureStore({
         playerInput: playerInputReducer,
         gameState: gameStateReducer
     }
-})
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
