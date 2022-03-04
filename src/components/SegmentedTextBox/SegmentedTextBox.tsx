bb9c4341328d04217ebcb15ca8ebc540bf1f10b1
Kobe Li <likobe23@gmail.com>
2022-03-03 19:04:16 -0800

    code quality

--- src/components/SegmentedTextBox/SegmentedTextBox.tsx
+++ src/components/SegmentedTextBox/SegmentedTextBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { InputBox } from "./InputBox";
 import { useAppDispatch, useKeyPress } from "../../store/hooks";
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore, getDefaultMiddleware, Middleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import playerInputReducer from './slices/playerInputSlice';
 import gameStateReducer from './slices/gameStateSlice';
 import { winLossCheckMiddleware } from './middleware';
